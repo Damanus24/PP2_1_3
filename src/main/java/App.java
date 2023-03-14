2c65fe3f759f90dc14122a50ad9cae2e9ebe528e
Dauren Amangaliev <amangaliev_dauren@mail.ru>
2023-03-14 23:28:43 +0300

    Выполнил задание

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat ben3 = (Cat) applicationContext.getBean("cat");
+        Cat ben4 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1 == bean2);
+        System.out.println(ben3 == ben4);
+
+//        System.out.println(bean.getMessage());
     }
 }
